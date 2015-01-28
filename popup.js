21d45e5f67953df3335f18400804db31f64817be
Mike Natale <xmike.natale@gmail.com>
2015-01-27 22:35:28 -0500

    Updated UI

--- popup.js
+++ popup.js
@@ -2,32 +2,50 @@
 $( document ).ready(function() {
 
 	$(".upvote").click(function(){		
-		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-			if(tabs[0].url.indexOf("reddit.com/user") >= 0){
-				chrome.tabs.executeScript({
-					code: 'upvote();'
-				});	
-			}
-		});			
+		if($(this).css("background-position") === "20px 0px"){
+			$(this).css("background-position", "0px 0px");
+			$(".downvote").css("background-position", "20px 0px");
+			chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+				if(tabs[0].url.indexOf("reddit.com/user") >= 0){
+					chrome.tabs.executeScript({
+						code: 'upvote();'
+					});	
+				}
+			});	
+		}
+		else{
+			$(this).css("background-position", "20px 0px");
+			chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+				if(tabs[0].url.indexOf("reddit.com/user") >= 0){
+					chrome.tabs.executeScript({
+						code: 'reset();'
+					});	
+				}
+			});
+		}
 	});
 	
 	$(".downvote").click(function(){
-		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-			if(tabs[0].url.indexOf("reddit.com/user") >= 0){
-				chrome.tabs.executeScript({
-					code: 'downvote();'
-				});	
-			}
-		});	
-	});
-	
-	$(".reset").click(function(){
-		chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-			if(tabs[0].url.indexOf("reddit.com/user") >= 0){
-				chrome.tabs.executeScript({
-					code: 'reset();'
-				});	
-			}
-		});	
+		if($(this).css("background-position") === "20px 0px"){
+			$(this).css("background-position", "0px 0px");
+			$(".upvote").css("background-position", "20px 0px");
+			chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+				if(tabs[0].url.indexOf("reddit.com/user") >= 0){
+					chrome.tabs.executeScript({
+						code: 'downvote();'
+					});	
+				}
+			});	
+		}
+		else{
+			$(this).css("background-position", "20px 0px");
+			chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+				if(tabs[0].url.indexOf("reddit.com/user") >= 0){
+					chrome.tabs.executeScript({
+						code: 'reset();'
+					});	
+				}
+			});
+		}
 	});
 });
--- upvotedownvote.js
+++ upvotedownvote.js
@@ -1,34 +1,35 @@
-var timer = 350;
+var uvdvi;
+var uvdvtimer = 350;
 
 function upvote(){
-	var i = 1;
+	uvdvi = 1;
 	$("#siteTable > .thing > .midcol > .arrow.up.login-required").each(function(){
 		var button = this;
-		setTimeout(function(){ button.click(); }, timer * i);
-		i++;
+		window.setTimeout(function(){ button.click(); }, uvdvtimer * uvdvi);
+		uvdvi++;
 	});
 }
 
 function downvote(){
-	var i = 1;
+	uvdvi = 1;
 	$("#siteTable > .thing > .midcol > .arrow.down.login-required").each(function(){
 		var button = this;
-		setTimeout(function(){ button.click(); }, timer * i);
-		i++;
+		window.setTimeout(function(){ button.click(); }, uvdvtimer * uvdvi);
+		uvdvi++;
 	});
 }
 
 function reset(){
-	var i = 1;
+	uvdvi = 1;
 	$("#siteTable > .thing > .midcol > .arrow.upmod.login-required").each(function(){
 		var button = this;
-		setTimeout(function(){ button.click(); }, timer * i);
-		i++;
+		window.setTimeout(function(){ button.click(); }, uvdvtimer * uvdvi);
+		uvdvi++;
 	});
 
 	$("#siteTable > .thing > .midcol > .arrow.downmod.login-required").each(function(){
 		var button = this;
-		setTimeout(function(){ button.click(); }, timer * i);
-		i++;
+		window.setTimeout(function(){ button.click(); }, uvdvtimer * uvdvi);
+		uvdvi++;
 	});
 }
